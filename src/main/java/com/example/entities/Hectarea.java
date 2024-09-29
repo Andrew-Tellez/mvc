1a4aecc683d2c50b8aef757098d8fb331e993cd6
andrew <andtr@protonmail.com>
2024-09-28 20:33:47 -0700

    feat: add getters and setters to Hectarea entity

--- src/main/java/com/example/entities/Hectarea.java
+++ src/main/java/com/example/entities/Hectarea.java
@@ -1,14 +1,51 @@
 package com.example.entities;
 
 public class Hectarea {
-  private int idHectarea, renta;
-  private String comunidad, ubicacion;
 
-  public Hectarea(int idHectarea, int renta, String comunidad, String ubicacion) {
-    this.idHectarea = idHectarea;
-    this.renta = renta;
-    this.comunidad = comunidad;
-    this.ubicacion = ubicacion;
-  }
+    private int idHectarea, renta;
+    private String comunidad, ubicacion;
 
+    public Hectarea(
+        int idHectarea,
+        int renta,
+        String comunidad,
+        String ubicacion
+    ) {
+        this.idHectarea = idHectarea;
+        this.renta = renta;
+        this.comunidad = comunidad;
+        this.ubicacion = ubicacion;
+    }
+
+    public int getIdHectarea() {
+        return idHectarea;
+    }
+
+    public void setIdHectarea(int idHectarea) {
+        this.idHectarea = idHectarea;
+    }
+
+    public int getRenta() {
+        return renta;
+    }
+
+    public void setRenta(int renta) {
+        this.renta = renta;
+    }
+
+    public String getComunidad() {
+        return comunidad;
+    }
+
+    public void setComunidad(String comunidad) {
+        this.comunidad = comunidad;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
 }
