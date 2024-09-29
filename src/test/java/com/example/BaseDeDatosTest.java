b8d03f325d137738fc60b11dd8a878ef8eefb64c
andrew <andtr@protonmail.com>
2024-09-28 20:34:51 -0700

    test: add unit tests to BaseDeDatos

---
+++ src/test/java/com/example/BaseDeDatosTest.java
@@ -0,0 +1,96 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.db.BaseDeDatos;
+import com.example.entities.Hectarea;
+import org.junit.jupiter.api.*;
+
+/**
+ * BaseDeDatosTest
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BaseDeDatosTest {
+
+    private BaseDeDatos BD;
+
+    @BeforeAll
+    public void setUp() {
+        BD = new BaseDeDatos();
+    }
+
+    @Test
+    public void testInsertarHectarea() {
+        int idHectarea = 1;
+        int renta = 100;
+        String comunidad = "comunidad";
+        String ubicacion = "ubicacion";
+        BD.insertarHectarea(
+            new Hectarea(idHectarea, renta, comunidad, ubicacion)
+        );
+        Hectarea hectarea = BD.recuperarHectarea(idHectarea);
+        assertEquals(idHectarea, hectarea.getIdHectarea());
+        assertEquals(renta, hectarea.getRenta());
+        assertEquals(comunidad, hectarea.getComunidad());
+        assertEquals(ubicacion, hectarea.getUbicacion());
+    }
+
+    @Test
+    public void testActualizarHectarea() {
+        int idHectarea = 2;
+        int renta = 100;
+        String comunidad = "comunidad";
+        String ubicacion = "ubicacion";
+        BD.insertarHectarea(
+            new Hectarea(idHectarea, renta, comunidad, ubicacion)
+        );
+        renta = 200;
+        comunidad = "comunidad2";
+        ubicacion = "ubicacion2";
+        BD.actualizarHectarea(
+            new Hectarea(idHectarea, renta, comunidad, ubicacion)
+        );
+        Hectarea hectarea = BD.recuperarHectarea(idHectarea);
+        assertEquals(idHectarea, hectarea.getIdHectarea());
+        assertEquals(renta, hectarea.getRenta());
+        assertEquals(comunidad, hectarea.getComunidad());
+        assertEquals(ubicacion, hectarea.getUbicacion());
+    }
+
+    @Test
+    public void testBorrarHectarea() {
+        int idHectarea = 3;
+        int renta = 100;
+        String comunidad = "comunidad";
+        String ubicacion = "ubicacion";
+        BD.insertarHectarea(
+            new Hectarea(idHectarea, renta, comunidad, ubicacion)
+        );
+        BD.borrarHectarea(idHectarea);
+        Hectarea hectarea = BD.recuperarHectarea(idHectarea);
+        assertNull(hectarea);
+    }
+
+    @Test
+    public void testRecuperarHectarea() {
+        int idHectarea = 4;
+        int renta = 100;
+        String comunidad = "comunidad";
+        String ubicacion = "ubicacion";
+        BD.insertarHectarea(
+            new Hectarea(idHectarea, renta, comunidad, ubicacion)
+        );
+        Hectarea hectarea = BD.recuperarHectarea(idHectarea);
+        assertEquals(idHectarea, hectarea.getIdHectarea());
+        assertEquals(renta, hectarea.getRenta());
+        assertEquals(comunidad, hectarea.getComunidad());
+        assertEquals(ubicacion, hectarea.getUbicacion());
+    }
+
+    @Test
+    public void testRecuperarHectareaNoExiste() {
+        int idHectarea = 5;
+        Hectarea hectarea = BD.recuperarHectarea(idHectarea);
+        assertNull(hectarea);
+    }
+}
