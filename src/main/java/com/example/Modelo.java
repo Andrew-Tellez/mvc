d1126c5077418528a73d0a12b25eff5e77ad37a5
andrew <andtr@protonmail.com>
2024-09-30 19:15:30 -0700

    feat(Modelo): add borrarHectarea method

--- src/main/java/com/example/Modelo.java
+++ src/main/java/com/example/Modelo.java
@@ -1,31 +1,33 @@
 package com.example;
+
+import com.example.db.BaseDeDatos;
 import com.example.entities.Hectarea;
+
 public class Modelo {
 
-  // TODO: La parte de manejar la base de datos tiene que ser pasada en el constructor;
-  // private BaseDeDatos bd;
+    private BaseDeDatos bd;
 
-  // public Modelo(BaseDeDatos bd) {
-  //   this.bd = bd;
-  // }
-  
-  public boolean guardarHectarea(Hectarea nuevaHectarea) {
-    return true;
-  }
+    public Modelo(BaseDeDatos bd) {
+        this.bd = bd;
+    }
 
-  public Hectarea recuperarHectarea(int idHectarea) {
-    return new Hectarea(idHectarea, idHectarea, null, null);
-  }
+    public boolean guardarHectarea(Hectarea nuevaHectarea) {
+        return true;
+    }
 
-  public boolean borrarHectarea(int idHectarea) {
-    return true;
-  }
+    public Hectarea recuperarHectarea(int idHectarea) {
+        return new Hectarea(idHectarea, idHectarea, null, null);
+    }
 
-  public Hectarea[] recuperarHectareas(int pagina) {
-    return new Hectarea[]{new Hectarea(pagina, pagina, null, null)};
-  }
-  
-  public boolean actualizarHectarea(Hectarea hectareaAModificar) {
-    return true;
-  }
+    public boolean borrarHectarea(int idHectarea) {
+        return bd.borrarHectarea(idHectarea) == 1;
+    }
+
+    public Hectarea[] recuperarHectareas(int pagina) {
+        return new Hectarea[] { new Hectarea(pagina, pagina, null, null) };
+    }
+
+    public boolean actualizarHectarea(Hectarea hectareaAModificar) {
+        return true;
+    }
 }
