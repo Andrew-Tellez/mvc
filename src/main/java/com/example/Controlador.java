277ed482e66d4df6623431af15ecf25037ae4d17
andrew <andtr@protonmail.com>
2024-10-01 18:58:31 -0700

    fix: update recuperar, borrar, actualizar

--- src/main/java/com/example/Controlador.java
+++ src/main/java/com/example/Controlador.java
@@ -1,11 +1,14 @@
 package com.example;
+
+import com.example.entities.Hectarea;
 import java.awt.event.*;
 import java.util.ArrayList;
 
-import com.example.entities.Hectarea;
 public class Controlador implements ActionListener {
+
     private Vista vista;
     private Modelo modelo;
+
     public Controlador(Vista vista, Modelo modelo) {
         this.modelo = modelo;
         this.vista = vista;
@@ -13,49 +16,56 @@ public class Controlador implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==vista.getBtnActualizar()){
+        if (e.getSource() == vista.getBtnActualizar()) {
             modelo.actualizarHectarea(vista.getHectarea());
             return;
         }
-        if(e.getSource()==vista.getBtnBorrar()){
+        if (e.getSource() == vista.getBtnBorrar()) {
             modelo.borrarHectarea(vista.getHectarea().getIdHectarea());
             return;
         }
-        if(e.getSource()==vista.getBtnConsultarTodas()){
+        if (e.getSource() == vista.getBtnConsultarTodas()) {
             int paginaActual = vista.getPagina();
-            ArrayList<Hectarea> hectareas = modelo.recuperarHectareas(paginaActual);
+            ArrayList<Hectarea> hectareas = modelo.recuperarHectareas(
+                paginaActual
+            );
             int totalHectareas = modelo.obtenerTotalHectareas();
             vista.setTotalPaginas(totalHectareas);
             vista.actualizarTabla(hectareas);
             vista.abrirModalConsultarTodas();
             return;
         }
-        if(e.getSource()==vista.getBtnGuardar()){
+        if (e.getSource() == vista.getBtnGuardar()) {
             modelo.guardarHectarea(vista.getHectarea());
             vista.limpiar();
             return;
         }
-        if(e.getSource()==vista.getBtnLimpiar()){
+        if (e.getSource() == vista.getBtnLimpiar()) {
             vista.limpiar();
             return;
         }
-        if(e.getSource()==vista.getBtnRecuperar()){
-            Hectarea hectareaRecuperada = modelo.recuperarHectarea(vista.getHectarea().getIdHectarea());
+        if (e.getSource() == vista.getBtnRecuperar()) {
+            Hectarea hectareaRecuperada = modelo.recuperarHectarea(
+                Integer.parseInt(vista.getTxtID().getText())
+            );
             vista.mostrarRecuperada(hectareaRecuperada);
             return;
         }
         if (e.getSource() == vista.getBtnSiguiente()) {
-          vista.setPagina(vista.getPagina() + 1);
-          int paginaActual = vista.getPagina();
-          ArrayList<Hectarea> hectareas = modelo.recuperarHectareas(paginaActual);
-          vista.actualizarTabla(hectareas);
+            vista.setPagina(vista.getPagina() + 1);
+            int paginaActual = vista.getPagina();
+            ArrayList<Hectarea> hectareas = modelo.recuperarHectareas(
+                paginaActual
+            );
+            vista.actualizarTabla(hectareas);
         }
         if (e.getSource() == vista.getBtnAnterior()) {
-          vista.setPagina(vista.getPagina() - 1);
-          int paginaActual = vista.getPagina();
-          ArrayList<Hectarea> hectareas = modelo.recuperarHectareas(paginaActual);
-          vista.actualizarTabla(hectareas);
+            vista.setPagina(vista.getPagina() - 1);
+            int paginaActual = vista.getPagina();
+            ArrayList<Hectarea> hectareas = modelo.recuperarHectareas(
+                paginaActual
+            );
+            vista.actualizarTabla(hectareas);
         }
-        
     }
 }
--- src/main/java/com/example/Vista.java
+++ src/main/java/com/example/Vista.java
@@ -1,9 +1,10 @@
 package com.example;
+
+import com.example.entities.Hectarea;
 import java.awt.BorderLayout;
 import java.awt.Font;
 import java.util.ArrayList;
 import java.util.Vector;
-
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -14,24 +15,25 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
-import com.example.entities.Hectarea;
-public class Vista extends JFrame{
+public class Vista extends JFrame {
+
     private JTextField txtID, txtComunidad, txtRenta, txtUbicacion;
     private JButton btnRecuperar, btnLimpiar, btnGuardar, btnActualizar, btnBorrar, btnConsultarTodas, btnAnterior, btnSiguiente;
     private DefaultTableModel modeloTabla;
     private JDialog modalConsultarTodas;
     private int paginaActual, totalPaginas;
 
-    public Vista(){
+    public Vista() {
         super("Catalogo hectareas");
         paginaActual = 1;
         totalPaginas = 0;
         hazInterfaz();
     }
+
     private void hazInterfaz() {
         setLayout(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(600,350);
+        setSize(600, 350);
         setResizable(false);
         setLocationRelativeTo(null);
         JLabel titulobl = new JLabel("Catalogo de Hectareas MangoDB");
@@ -54,7 +56,7 @@ public class Vista extends JFrame{
         modalConsultarTodas.setSize(650, 350);
         modalConsultarTodas.setTitle("Consultar todas");
         modalConsultarTodas.setLocationRelativeTo(null);
-        String cols[] = {"ID", "Comunidad", "Renta", "Ubicacion"};
+        String cols[] = { "ID", "Comunidad", "Renta", "Ubicacion" };
         JTable tabla = new JTable();
         JScrollPane scrollPane = new JScrollPane(tabla);
         modeloTabla = new DefaultTableModel();
@@ -71,29 +73,29 @@ public class Vista extends JFrame{
         modalConsultarTodas.add(panel, BorderLayout.SOUTH);
 
         {
-            titulobl.setBounds((getWidth()/2)-150, 10, 300, 30);
+            titulobl.setBounds((getWidth() / 2) - 150, 10, 300, 30);
             IDlbl.setBounds(30, 50, 200, 25);
             txtID.setBounds(230, 50, 300, 25);
-    
+
             Comunidadlbl.setBounds(30, 90, 200, 25);
             txtComunidad.setBounds(230, 90, 300, 25);
-    
+
             Rentalbl.setBounds(30, 130, 200, 25);
             txtRenta.setBounds(230, 130, 300, 25);
-    
+
             Ubibl.setBounds(30, 170, 200, 25);
             txtUbicacion.setBounds(230, 170, 300, 25);
-    
+
             btnRecuperar.setBounds(30, 210, 150, 30);
             btnLimpiar.setBounds(210, 210, 150, 30);
             btnGuardar.setBounds(390, 210, 150, 30);
-    
+
             btnActualizar.setBounds(30, 250, 150, 30);
             btnBorrar.setBounds(210, 250, 150, 30);
             btnConsultarTodas.setBounds(390, 250, 150, 30);
         }
-        Font fuente1 = new Font("Arial", 1,18); 
-        Font fuente2 = new Font("Arial", 1,15);
+        Font fuente1 = new Font("Arial", 1, 18);
+        Font fuente2 = new Font("Arial", 1, 15);
         titulobl.setFont(fuente1);
         IDlbl.setFont(fuente2);
         Comunidadlbl.setFont(fuente2);
@@ -116,42 +118,53 @@ public class Vista extends JFrame{
         add(btnConsultarTodas);
         setVisible(true);
     }
-    public Hectarea getHectarea(){
-        return new Hectarea(Integer.parseInt(txtID.getText()),Integer.parseInt(txtRenta.getText()),txtComunidad.getText(),txtUbicacion.getText());
+
+    public Hectarea getHectarea() {
+        return new Hectarea(
+            Integer.parseInt(txtID.getText()),
+            Integer.parseInt(txtRenta.getText()),
+            txtComunidad.getText(),
+            txtUbicacion.getText()
+        );
     }
+
     public void mostrarRecuperada(Hectarea hectareaRecuperada) {
-        txtID.setText(""+hectareaRecuperada.getIdHectarea());
-        txtComunidad.setText(""+hectareaRecuperada.getComunidad());
-        txtRenta.setText(""+hectareaRecuperada.getRenta());
-        txtUbicacion.setText(""+hectareaRecuperada.getUbicacion());
+        txtID.setText("" + hectareaRecuperada.getIdHectarea());
+        txtComunidad.setText("" + hectareaRecuperada.getComunidad());
+        txtRenta.setText("" + hectareaRecuperada.getRenta());
+        txtUbicacion.setText("" + hectareaRecuperada.getUbicacion());
         txtID.requestFocus();
     }
+
     public void abrirModalConsultarTodas() {
-      modalConsultarTodas.setVisible(true);
+        modalConsultarTodas.setVisible(true);
     }
-    @SuppressWarnings({"unchecked", "rawtypes"})
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
     public void actualizarTabla(ArrayList<Hectarea> hectareas) {
-      modeloTabla.setRowCount(0);
-      Vector fila;
-      for (int i = 0; i < hectareas.size(); i++) {
-        Hectarea hectareaActual = hectareas.get(i);
+        modeloTabla.setRowCount(0);
+        Vector fila;
+        for (int i = 0; i < hectareas.size(); i++) {
+            Hectarea hectareaActual = hectareas.get(i);
 
-        fila = new Vector<>();
-        fila.add(hectareaActual.getIdHectarea());
-        fila.add(hectareaActual.getComunidad());
-        fila.add(hectareaActual.getRenta());
-        fila.add(hectareaActual.getUbicacion());
+            fila = new Vector<>();
+            fila.add(hectareaActual.getIdHectarea());
+            fila.add(hectareaActual.getComunidad());
+            fila.add(hectareaActual.getRenta());
+            fila.add(hectareaActual.getUbicacion());
 
-        modeloTabla.addRow(fila);
-      }
-      btnAnterior.setEnabled(paginaActual != 1);
-      btnSiguiente.setEnabled(paginaActual < totalPaginas);
-      modalConsultarTodas.update(modalConsultarTodas.getGraphics());
+            modeloTabla.addRow(fila);
+        }
+        btnAnterior.setEnabled(paginaActual != 1);
+        btnSiguiente.setEnabled(paginaActual < totalPaginas);
+        modalConsultarTodas.update(modalConsultarTodas.getGraphics());
     }
+
     public void setTotalPaginas(int totalHectareas) {
-      int totalPaginas = (int) Math.ceil((double) totalHectareas / 5);
-      this.totalPaginas = totalPaginas;
+        int totalPaginas = (int) Math.ceil((double) totalHectareas / 5);
+        this.totalPaginas = totalPaginas;
     }
+
     public void limpiar() {
         txtID.setText("");
         txtComunidad.setText("");
@@ -159,6 +172,7 @@ public class Vista extends JFrame{
         txtUbicacion.setText("");
         txtID.requestFocus();
     }
+
     public void setControlador(Controlador controlador) {
         btnRecuperar.addActionListener(controlador);
         btnLimpiar.addActionListener(controlador);
@@ -169,58 +183,76 @@ public class Vista extends JFrame{
         btnSiguiente.addActionListener(controlador);
         btnAnterior.addActionListener(controlador);
     }
+
     public void setTxtID(JTextField txtID) {
         this.txtID = txtID;
     }
+
     public JTextField getTxtComunidad() {
         return txtComunidad;
     }
+
     public void setTxtComunidad(JTextField txtComunidad) {
         this.txtComunidad = txtComunidad;
     }
+
     public JTextField getTxtRenta() {
         return txtRenta;
     }
+
     public void setTxtRenta(JTextField txtRenta) {
         this.txtRenta = txtRenta;
     }
+
     public JTextField getTxtUbicacion() {
         return txtUbicacion;
     }
+
     public void setTxtUbicacion(JTextField txtUbicacion) {
         this.txtUbicacion = txtUbicacion;
     }
+
     public JButton getBtnRecuperar() {
         return btnRecuperar;
     }
+
     public JButton getBtnLimpiar() {
         return btnLimpiar;
     }
+
     public JButton getBtnGuardar() {
         return btnGuardar;
     }
+
     public JButton getBtnActualizar() {
         return btnActualizar;
     }
+
     public JButton getBtnBorrar() {
         return btnBorrar;
     }
+
     public JButton getBtnConsultarTodas() {
         return btnConsultarTodas;
     }
+
     public JTextField getTxtID() {
         return txtID;
     }
+
     public JButton getBtnAnterior() {
-      return btnAnterior;
+        return btnAnterior;
     }
+
     public JButton getBtnSiguiente() {
-      return btnSiguiente;
+        return btnSiguiente;
     }
+
     public int getPagina() {
-      return paginaActual;
+        return paginaActual;
     }
+
     public void setPagina(int nuevaPagina) {
-      paginaActual = nuevaPagina;
+        paginaActual = nuevaPagina;
     }
 }
